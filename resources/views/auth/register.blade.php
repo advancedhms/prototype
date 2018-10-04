dc218ab6e3ff4f31286b2c83f8b4862c33c2ae88
Advancedhms <advancedhmsgroup@gmail.com>
2018-10-04 21:23:19 +0000

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,17 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+                        <div class="form-group row">
+
+                            <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                            <input name="is_admin" class="form-check-input" type="checkbox" value="" id="defaultCheck1" checked disabled>
+                                            <label class="form-check-label" for="defaultCheck1">
+                                             Register as Admin
+                                            </label>
+                                    </div>
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
