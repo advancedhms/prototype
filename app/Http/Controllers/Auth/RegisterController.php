ee152b4f0f0228c6c19dfb74c6da182d1963d027
Advancedhms <advancedhmsgroup@gmail.com>
2018-10-27 19:05:51 +0000

    new

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'is_admin' => isset($data['is_admin']) ? true : false,
+            'is_admin' => isset($data['is_admin']) ? true : true,
             'password' => Hash::make($data['password']),
         ]);
     }
