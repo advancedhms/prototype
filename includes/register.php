54df0c788c010b9092a7e9b55f6ca1756218c050
Nii Adotei <addoteyd@gmail.com>
2018-10-29 12:12:16 +0000

    register file changed

--- includes/register.php
+++ includes/register.php
@@ -1,42 +1,58 @@
-<?PHP
+<?php
+session_start();
 
-require_once("formvalidator.php");
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array();
 
-class Adminreg
-{
-  protected $_email;    // using protected so they can be accessed
-  protected $_password; // and overidden if necessary
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'hms');
 
-  protected $_db;       // stores the database handler
-  protected $_user;     // stores the user data
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  protected $errors = NULL; //array to hold all of the errors
-  //Defining the various errors
-  const ERROR1 = "USER Exists";
-  const ERROR2 = "password too short";
-  const ERROR3 = "userid not defined";
-
-  public $error_message = '';
-
-  public function __construct()
-  {
-     $this->_db = Database::getInstance();
-     session_start();
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
   }
 
-function RegisterUser()
-{
-    if(!isset($_POST['submitted']))
-    {
-       return false;
+  // first check the database to make sure
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM hms WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
 
-    $formvars = array();
-
-    if(!$this->ValidateRegistrationSubmission())
-    {
-        return false;
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
     }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO admin (fullname,username, email,hospitalid, password)
+  			  VALUES('$fullname','$username', '$email','$hospitalid' '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: admin.php');
   }
 }
-?>
+
+// ...
