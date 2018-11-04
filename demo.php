5f8c8a80257f56079353fd62ad4127a33c497183
Advancedhms <advancedhmsgroup@gmail.com>
2018-11-04 08:46:11 +0000

    changes

--- demo.php
+++ demo.php
@@ -1,15 +1,15 @@
 <?php 
 	require 'includes/classes.php';
-
+  session_start();
 	//$user = new User;
 	//echo "<tt><pre>".var_export($user, true)."</pre></tt>";
 
 	    $data = array(
-	    'username' => 'admin',
-        'name' => 'Isaac Botwe',
-        'email' => 'isaacbotwe7@gmail.com',
-        'hospitalid' => 'hosp1234',
-        
+	    'id' => 'ikeadmin',
+        'name' => 'Kwame Owusu',
+        'email' => 'nanayawasuako@gmail.com',
+        'hospitalid' => 'hosp1214',
+        'password' => '2bhjsodir',        
       );
 
      /* $new_user = $user->register($data);
@@ -27,27 +27,33 @@
         'hospitalid' => 'hosp1234',
         
       ));*/
-      $new_user = new Admin(array(
-        'username' => 'isaacadmin',
-        'name' => 'Isaac Asuako',
-        'email' => 'isaacbotwe@gmail.com',
-        'hospitalid' => 'hosp1234',
-        
-      ));
+      $new_user = new Admin;
     
      /*$new_user->username = $data['username'];
      $new_user->name = $data['name'];
      $new_user->email=$data['email'];
      $new_user->hospitalid=$data['hospitalid'];
      $new_user->save();
-*/
+
       echo "<tt><pre>".var_export($new_user, true)."</pre></tt>";
       echo $new_user->display();
+      echo "<br/>";
       $user = $new_user->saveDetails();
       if($user) {
         echo "Registration Succesfull";
       }
       else {
         echo "Registeration Unsuccessful";
+      }
+      echo "<br/>";*/
+      $new_admin_user = new User($data);
+
+      echo "<tt><pre>". var_export($new_admin_user, true)   ."</pre></tt>";
+      $user = $new_admin_user->register();
+      if($user && $data['id'] == $_SESSION['user_id']) {
+        header('Location: admin.php');
+      }
+      else {
+        echo "Registeration unsuccesfull";
       }
 ?>
